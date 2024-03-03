e05aae7aea72f410b968d5a5e7388d156139333b
PRIYTESH PRASAD <61977852+priyteshprasad@users.noreply.github.com>
2024-03-03 19:12:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function handleInput(element, event) {
+      if (event.inputType === 'deleteContentBackward' && element.previousElementSibling) {
+        // On backspace, move focus to the previous input
+        element.previousElementSibling.focus();
+      } else if (element.nextElementSibling) {
+        // On input, move focus to the next input
+        element.nextElementSibling.focus();
+      }
+    }
